70934153f119f5d579d12d580726662c4f6b2275
DavidGuerreroLacharme <judagela@gmail.com>
2023-07-31 19:18:41 -0500

    .

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,8 +14,8 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Category::create([
-            'name' => 'T-Shirts',
-            'slug' => 't-shirts',
+            'name' => 'Espresso',
+            'slug' => 'espresso',
         ]);
     }
 }
